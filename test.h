76a028d5ee1cf2baf337c41e8f0e83860d6b3746
di <git@diferrari.com>
2026-02-03 00:00:00 +0000

    [TEST] parentheses in asserts

--- test.h
+++ test.h
@@ -15,8 +15,8 @@ static inline bool assert_true_func(bool condition, char *fmt, ...){
     return false;
 }
 
-#define assert_true(cond, fmt, ...) if (!assert_true_func(cond, fmt, ##__VA_ARGS__)) return false;
-#define assert_false(cond, fmt, ...) if (!assert_true_func(!cond, fmt, ##__VA_ARGS__)) return false;
-#define assert_eq(el1, el2, fmt, ...) if (!assert_true_func(el1 == el2, fmt, ##__VA_ARGS__)) return false;
-#define assert_neq(el1, el2, fmt, ...) if (!assert_true_func(el1 != el2, fmt, ##__VA_ARGS__)) return false;
+#define assert_true(cond, fmt, ...) if (!assert_true_func((cond), fmt, ##__VA_ARGS__)) return false;
+#define assert_false(cond, fmt, ...) if (!assert_true_func(!(cond), fmt, ##__VA_ARGS__)) return false;
+#define assert_eq(el1, el2, fmt, ...) if (!assert_true_func((el1) == (el2), fmt, ##__VA_ARGS__)) return false;
+#define assert_neq(el1, el2, fmt, ...) if (!assert_true_func((el1) != (el2), fmt, ##__VA_ARGS__)) return false;
 #define test_fail(fmt, ...) if (!assert_true_func(false, fmt, ##__VA_ARGS__)) return false;
