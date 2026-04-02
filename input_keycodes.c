620527cb24f55b3010488017e90597c869483075
di <git@diferrari.com>
2026-04-02 00:00:00 +0000

    [KBD] added shift-modifier characters [BUFFER] remove text function [BUFFER] fix write_to

--- input_keycodes.c
+++ input_keycodes.c
@@ -1,5 +1,10 @@
 #include "input_keycodes.h"
 
-char hid_to_char(unsigned char c){
-    return hid_keycode_to_char[c];
+char hid_to_char(unsigned char c, u8 modifier){
+    switch (modifier) {
+        case KEY_MOD_LSHIFT:
+            return hid_keycode_to_shift_char[c];
+        default:
+            return hid_keycode_to_char[c];
+    }
 }
