c19c5d367263fe7348e315fa508ea16024be0a75
di <git@diferrari.com>
2026-04-26 00:00:00 +0000

    [VFS] filesystem transformations

--- files/vfs.h
+++ files/vfs.h
@@ -87,13 +87,14 @@ static inline size_t list_entries(u64 *offset, fs_dir_list_helper *helper){
 
 static inline string_slice first_path_component(const char *path){
     const char *next = seek_to(path, '/');
-    return (string_slice){ .data = (char*)path, .length = next - path - (*next ? 1 : 0)};
+    return (string_slice){ .data = (char*)path, .length = next - path - (*(next-1) == '/' ? 1 : 0)};
 }
 
 static inline FS_RESULT vfs_open(const char *path, file *fd){
     string_slice first = first_path_component(!path || !strlen(path) ? DIR_AS_FILE : path+1);
     module_file *mfile = eval_entry(first);
     if (!mfile) return FS_RESULT_NOTFOUND;
+    if (mfile->actions.open) return mfile->actions.open(path,fd);
     mfile->references++;
     
     if (mfile->alias_info.alias_path.length){
@@ -111,7 +112,6 @@ static inline FS_RESULT vfs_open(const char *path, file *fd){
         }
         memcpy(fd, &mfile->alias_info.alias_fd, sizeof(file));
         fd->id = mfile->fid;
-        //TODO: where do transforms fit in this
         return FS_RESULT_SUCCESS;
     }
     
@@ -120,7 +120,6 @@ static inline FS_RESULT vfs_open(const char *path, file *fd){
         return FS_RESULT_DRIVER_ERROR;
     }
     
-    if (mfile->actions.open) mfile->actions.open(path,fd);
     //TODO: bindings for aliases
     fd->id = mfile->fid;
     return FS_RESULT_SUCCESS;
@@ -129,26 +128,26 @@ static inline FS_RESULT vfs_open(const char *path, file *fd){
 static inline size_t vfs_read(file *fd, char* buf, size_t size, file_offset offset){
     module_file *mfile = find_entry(fd->id);
     if (!mfile) return 0;
+    if (mfile->actions.read) return mfile->actions.read(fd, buf, size, offset);
     if (mfile->alias_info.alias_fd.id){
         size_t res = readf(&mfile->alias_info.alias_fd, buf, size);
         memcpy(fd, &mfile->alias_info.alias_fd, sizeof(file));
         fd->id = mfile->fid;
         return res;
     } 
-    if (mfile->actions.read) return mfile->actions.read(fd, buf, size, offset);
     return buffer_read(&mfile->file_buffer, buf, size, offset);
 }
 
 static inline size_t vfs_write(file *fd, const char* buf, size_t size, file_offset offset){
     module_file *mfile = find_entry(fd->id);
     if (!mfile) return 0;
+    if (mfile->actions.write) return mfile->actions.write(fd, (void*)buf, size, offset);
     if (mfile->alias_info.alias_fd.id){
         size_t res = writef(&mfile->alias_info.alias_fd, buf, size);
         memcpy(fd, &mfile->alias_info.alias_fd, sizeof(file));
         fd->id = mfile->fid;
         return res;
     } 
-    if (mfile->actions.write) return mfile->actions.write(fd, (void*)buf, size, offset);
     return buffer_write_to(&mfile->file_buffer, buf, size, fd->cursor);
 }
 
@@ -162,6 +161,7 @@ static inline bool vfs_stat(const char *path, fs_stat *out_stat){
             return stat_dir(out_stat);
         return 0;
     }
+    if (file->actions.getstat) file->actions.getstat(path, out_stat);
     if (file->alias_info.alias_path.length){
         string fullpath = file->alias_info.alias_path;
         string additional = (string){};
@@ -174,7 +174,6 @@ static inline bool vfs_stat(const char *path, fs_stat *out_stat){
         string_free(additional);
         return res;
     } 
-    if (file->actions.getstat) file->actions.getstat(path, out_stat);
     out_stat->size = file->file_buffer.buffer_size;
     out_stat->type = file->entry_type;
     out_stat->data_type = file->data_type;
@@ -184,12 +183,12 @@ static inline bool vfs_stat(const char *path, fs_stat *out_stat){
 static inline void vfs_close(file *fd){
     module_file *mfile = find_entry(fd->id);
     if (!mfile) return;
-    if (mfile->alias_info.alias_fd.id){
-        return closef(&mfile->alias_info.alias_fd);
-    }
     if (mfile->actions.close){
         mfile->actions.close(fd);
         return;
+    }
+    if (mfile->alias_info.alias_fd.id){
+        return closef(&mfile->alias_info.alias_fd);
     }
 } 
 
@@ -199,6 +198,8 @@ static inline size_t vfs_readdir(const char *path, void *buf, size_t size, file_
     if (*(path + first.length) == 0)
         return list_entries(offset, &helper);
     module_file *file = eval_entry(first);
+    if (!file) return 0;
+    if (file->actions.readdir) return file->actions.readdir(path, buf, size, offset);
     if (file->entry_type != entry_directory){
         print("%s is not a directory",path);
         return 0;
