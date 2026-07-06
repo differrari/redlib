c2ce397061e0034663cea8dbb46936af19a7e15e
di <git@diferrari.com>
2026-07-06 00:00:00 +0000

    [JOB] file descriptor support [JOB] all fs op types [JOB] copy as flags

--- files/jobs.h
+++ files/jobs.h
@@ -7,18 +7,18 @@ typedef u64 job_id_t;
 
 typedef enum {
     job_none,
-    // job_open,
-    // job_read,
-    // job_write,
-    // job_trunc,
-    // job_close,
+    job_open,
+    job_read,
+    job_write,
+    job_trunc,
+    job_close,
     job_stat,
     job_readdir
 } job_types;
 
 #define MAX_JOB_BUFFERS 8
 
-typedef enum { copy_on_start, copy_on_end } job_sync_type;
+typedef enum { copy_on_start = 1 << 0, copy_on_end = 1 << 1 } job_sync_type;
 
 typedef struct {
     sizedptr worker_ptr;
@@ -26,6 +26,7 @@ typedef struct {
     u8 arg_num;
     bool explicit_size;
     job_sync_type sync;
+    bool fd;
 } job_buffer;
 
 typedef struct {
@@ -33,7 +34,6 @@ typedef struct {
     uptr requesting_pid;
     u16 worker_pid;
     job_types type;
-    file fd;
     job_buffer buffers[8];
     size_t buffer_count;
     //buffers
