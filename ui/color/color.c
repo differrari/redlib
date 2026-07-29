e00a1eec1804238081362a12496b07603fcf0ef6
di <git@diferrari.com>
2026-07-29 00:00:00 +0000

    [ALLOC] print errors on failure [COLOR, WIP] saturate color

--- ui/color/color.c
+++ ui/color/color.c
@@ -15,3 +15,18 @@ color complementary_color(color base){
     u8 b = 255 - ((base << 16) & 0xFF);
     return (0xFF << 24) | (r << 16) | (g << 8) | b; 
 }
+
+argbcolor saturate_argb(argbcolor col, u32 t){
+    //TODO: t seems to be getting interpreted as the opposite of what it should. Probably because of module. This function was originally desaturate
+    u32 i = ((col.red + col.green + col.blue)/3) % 256;
+
+    u32 dr = i - col.red;
+    u32 dg = i - col.green;
+    u32 db = i - col.blue;
+
+    u32 r = col.red     + dr * t;
+    u32 g = col.green   + dg * t;
+    u32 b = col.blue    + db * t;
+
+    return (argbcolor){.red = r % 256, .green = g % 256, .blue = b % 256, .alpha = col.alpha};
+}
--- ui/color/color.h
+++ ui/color/color.h
@@ -4,3 +4,8 @@
 
 color text_color_for_base(color base);
 color complementary_color(color base);
+argbcolor saturate_argb(argbcolor col, u32 t);
+
+static inline u32 saturate(color col, u32 t){
+    return saturate_argb((argbcolor){.color = col}, t).color;
+}
