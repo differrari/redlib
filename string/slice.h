53a1158df9706a84dd9e24facd6de1cc18d4de7f
di <git@diferrari.com>
2026-08-06 00:00:00 +0000

    [SLICE] index fix

--- string/slice.h
+++ string/slice.h
@@ -43,9 +43,9 @@ static inline sizedptr slice_to_sizedptr(string_slice slice){
 }
 
 static inline int slice_index(string_slice a, const char *b){
-    for (int i = 0; a.length; i++){
+    for (int i = 0; i < a.length; i++){
         int j = 0;
-        while (b[j] && a.data[i + j] == b[j]) j++;
+        while (b[j] && i+j < a.length && a.data[i + j] == b[j]) j++;
         if (!b[j]) return i;
     }
     return -1;
